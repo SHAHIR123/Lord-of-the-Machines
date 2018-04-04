4fcf91ad017a8f3fd070fd7dbcefd2a2c3bbc972
SHAHIR123 <kshahir2004@yahoo.co.in>
2018-04-04 22:29:26 +0300

    first commit

---
+++ ExtraTree_Model.py
@@ -0,0 +1,69 @@
+#Load libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import cross_val_score
+
+
+# Load Data Set
+
+train = pd.read_csv("train_HFxi8kT/train.csv")
+campaign_data = pd.read_csv("train_HFxi8kT/campaign_data.csv")
+test = pd.read_csv("test_BDIfz5B.csv/test_BDIfz5B.csv")
+
+# Merge Data sets for feature engineering
+
+df_train = pd.merge(train, campaign_data, how='left', on='campaign_id', left_on=None, right_on=None,
+                          left_index=False, right_index=False, sort=True,
+                          suffixes=('_x', '_y'), copy=True, indicator=False)
+
+
+df_test = pd.merge(test, campaign_data, how='left', on='campaign_id', left_on=None, right_on=None,
+                          left_index=False, right_index=False, sort=True,
+                          suffixes=('_x', '_y'), copy=True, indicator=False)
+
+# convert catogorical variable
+
+le = LabelEncoder()
+
+le.fit(np.hstack([df_train.communication_type, df_test.communication_type]))
+df_train.communication_type = le.transform(df_train.communication_type)
+df_test.communication_type = le.transform(df_test.communication_type)
+del le
+
+# Select Predictors and prdiction columns
+
+predictors = df_train[["user_id", "campaign_id", "communication_type", "total_links","no_of_internal_links", "no_of_images",
+                   "no_of_sections"]]
+
+y = df_train["is_click"]
+
+
+# Creat Model
+
+model = ExtraTreesClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
+
+# Cross Validation
+
+scores = cross_val_score(model, predictors, y, scoring='roc_auc', cv=5)
+print(scores)
+
+# Select predictors from test columns
+
+test_predictors = df_test[["user_id", "campaign_id", "communication_type", "total_links","no_of_internal_links", "no_of_images",
+                   "no_of_sections"]]
+
+# Fit Model
+
+model.fit(predictors, y)
+
+# prediction
+
+prediction = model.predict(test_predictors)
+
+# Creat submission file
+
+my_submission = pd.DataFrame({'id': test.id, 'is_click': prediction})
+my_submission.to_csv('RF_FE_submission.csv', index=False)
---
+++ README.md
@@ -0,0 +1,44 @@
+# LOARD OF MACHINES - Data Science Hackathon
+## a data science / machine learning hackathon designed to discover the best data scientists in the community.
+
+* Problem Statement
+
+
+Email Marketing is still the most successful marketing channel and the essential element of any digital marketing strategy. Marketers spend a lot of time in writing that perfect email, labouring over each word, catchy layouts on multiple devices to get them best in-industry open rates & click rates.
+
+How can I build my campaign to increase the click-through rates of email? - a question that is often heard when marketers are creating their email marketing plans.
+
+Can we optimize our email marketing campaigns with Data Science?
+
+It's time to unlock marketing potential and build some exceptional data-science products for email marketing.
+
+Analytics Vidhya sends out marketing emailers for various events such as conferences, hackathons, etc. We have provided a sample of user-email interaction data from July 2017 to December 2017. You are required to predict the click probability of links inside a mailer for email campaigns from January 2018 to March 2018.
+
+Data set can be found on [Analytics Vidya](https://datahack.analyticsvidhya.com/contest/lord-of-the-machines/)
+
+### DATASET DESCRIPTION
+
+* campaign_data.csv
+
+Contains the features related to 52 email Campaigns
+
+* train.csv
+
+Contains the click and open information for each user corresponding to given campaign id (Jul 17 - Dec 17)
+
+* test.csv
+
+Contains the user and campaigns for which is_click needs to be predicted (Jan 18 - Mar 18)
+
+* EVALUATION METRIC
+
+The evaluation metric for this competition is AUC-ROC score.
+
+* PUBLIC AND PRIVATE SPLIT
+
+Note that the test data is further randomly divided into Public (30%) and Private (70%) data. Your initial responses will be checked and scored on the Public data.
+
+The final rankings would be based on your private score which will be published once the competition is over.
+
+* I have been ranked 147 among 3594 contestents
+
